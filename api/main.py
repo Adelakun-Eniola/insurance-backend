47b291747683864ff810afcc03d76f5868deeafd
boylax23 <eniolaadelakun2005@gmail.com>
2025-05-16 12:14:10 +0100

    allowed cors

--- api/main.py
+++ api/main.py
@@ -12,7 +12,7 @@ import uvicorn
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
